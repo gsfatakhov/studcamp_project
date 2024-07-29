aa953f8c46316aaf40dba4b1bd111fca79580ecb
Georgii <fatahoff.georgy@yandex.ru>
2024-07-29 22:40:17 +0300

    init

---
+++ layers.py
@@ -0,0 +1,137 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+# Graph Convolutional Layer:
+class GCNLayer(nn.Module):
+    def __init__(self, in_dim, out_dim):
+        super().__init__()
+
+        self.dense = nn.Linear(in_dim, out_dim)
+
+    def forward(self, adj, X):
+        adj = adj + torch.eye(adj.size(0)).to(adj.device)
+        h = self.dense(X)
+        norm = adj.sum(1) ** (-1 / 2)
+        h = norm[None, :] * adj * norm[:, None] @ h
+        return h
+
+
+# A = ReLu(W)    
+class Graph_ReLu_W(nn.Module):
+    def __init__(self, num_nodes, k, device):
+        super(Graph_Relu_W, self).__init__()
+        self.num_nodes = num_nodes
+        self.k = k
+
+        self.A = nn.Parameter(torch.randn(num_nodes, num_nodes).to(device), requires_grad=True).to(device)
+
+    def forward(self, idx):
+        adj = F.relu(self.A)
+
+        if self.k:
+            mask = torch.zeros(idx.size(0), idx.size(0)).to(self.device)
+            mask.fill_(float('0'))
+            s1, t1 = (adj + torch.rand_like(adj) * 0.01).topk(self.k, 1)
+            mask.scatter_(1, t1, s1.fill_(1))
+            adj = adj * mask
+
+        return adj
+
+
+# A for Directed graphs:
+class Graph_Directed_A(nn.Module):
+
+    def __init__(self, num_nodes, window_size, alpha, k, device):
+        super(Graph_Directed_A, self).__init__()
+
+        self.alpha = alpha
+        self.k = k
+        self.device = device
+
+        self.e1 = nn.Embedding(num_nodes, window_size)
+        self.e2 = nn.Embedding(num_nodes, window_size)
+        self.l1 = nn.Linear(window_size, window_size)
+        self.l2 = nn.Linear(window_size, window_size)
+
+    def forward(self, idx):
+        m1 = torch.tanh(self.alpha * self.l1(self.e1(idx)))
+        m2 = torch.tanh(self.alpha * self.l2(self.e2(idx)))
+        adj = F.relu(torch.tanh(self.alpha * torch.mm(m1, m2.transpose(1, 0))))
+
+        if self.k:
+            mask = torch.zeros(idx.size(0), idx.size(0)).to(self.device)
+            mask.fill_(float('0'))
+            s1, t1 = (adj + torch.rand_like(adj) * 0.01).topk(self.k, 1)
+            mask.scatter_(1, t1, s1.fill_(1))
+            adj = adj * mask
+
+        return adj
+
+
+# A for Uni-directed graphs:
+class Graph_Uni_Directed_A(nn.Module):
+
+    def __init__(self, num_nodes, window_size, alpha, k, device):
+        super(Graph_Directed_A, self).__init__()
+
+        self.alpha = alpha
+        self.k = k
+        self.device = device
+
+        self.e1 = nn.Embedding(num_nodes, window_size)
+        self.e2 = nn.Embedding(num_nodes, window_size)
+        self.l1 = nn.Linear(window_size, window_size)
+        self.l2 = nn.Linear(window_size, window_size)
+
+    def forward(self, idx):
+        m1 = torch.tanh(self.alpha * self.l1(self.e1(idx)))
+        m2 = torch.tanh(self.alpha * self.l2(self.e2(idx)))
+        adj = F.relu(torch.tanh(self.alpha * (torch.mm(m1, m2.transpose(1, 0)) - torch.mm(m2, m1.transpose(1, 0)))))
+
+        if self.k:
+            mask = torch.zeros(idx.size(0), idx.size(0)).to(self.device)
+            mask.fill_(float('0'))
+            s1, t1 = (adj + torch.rand_like(adj) * 0.01).topk(self.k, 1)
+            mask.scatter_(1, t1, s1.fill_(1))
+            adj = adj * mask
+
+        return adj
+
+
+# A for Undirected graphs:
+class Graph_Undirected_A(nn.Module):
+
+    def __init__(self, num_nodes, window_size, alpha, k, device):
+        super(Graph_Directed_A, self).__init__()
+
+        self.alpha = alpha
+        self.k = k
+        self.device = device
+
+        self.e1 = nn.Embedding(num_nodes, window_size)
+        self.l1 = nn.Linear(window_size, window_size)
+
+    def forward(self, idx):
+        m1 = torch.tanh(self.alpha * self.l1(self.e1(idx)))
+        m2 = torch.tanh(self.alpha * self.l1(self.e1(idx)))
+        adj = F.relu(torch.tanh(self.alpha * torch.mm(m1, m2.transpose(1, 0))))
+
+        if self.k:
+            mask = torch.zeros(idx.size(0), idx.size(0)).to(self.device)
+            mask.fill_(float('0'))
+            s1, t1 = (adj + torch.rand_like(adj) * 0.01).topk(self.k, 1)
+            mask.scatter_(1, t1, s1.fill_(1))
+            adj = adj * mask
+
+        return adj
+
+
+class ConstantGraph(nn.Module):
+    def init(self, constant_adj_matrix):
+        super(ConstantGraph, self).init()
+        self.constant_adj_matrix = constant_adj_matrix
+
+    def forward(self, idx):
+        return self.constant_adj_matrix
---
+++ models.py
@@ -0,0 +1,77 @@
+import numpy as np
+import pandas as pd
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from layers import *
+
+
+class GNNTEP(nn.Module):
+    def __init__(self, nnodes, window_size, ngnn, gsllayer, nhidden, alpha, k, out_channels, device,
+                 constant_adj_matrix=None):
+        super(GNNTEP, self).__init__()
+        self.window_size = window_size
+        self.nhidden = nhidden
+        self.nnodes = nnodes
+        self.device = device
+        self.idx = torch.arange(self.nnodes).to(device)
+        self.adj = [0 for i in range(ngnn)]
+        self.h = [0 for i in range(ngnn)]
+        self.skip = [0 for i in range(ngnn)]
+        self.z = (torch.ones(nnodes, nnodes) - torch.eye(nnodes)).to(device)
+        self.ngnn = ngnn
+
+        self.graph_struct = nn.ModuleList()
+        self.conv1 = nn.ModuleList()
+        self.bnorm1 = nn.ModuleList()
+        self.conv2 = nn.ModuleList()
+        self.bnorm2 = nn.ModuleList()
+
+        if constant_adj_matrix is not None:
+            self.constant_adj_matrix = torch.tensor(constant_adj_matrix, device=device, dtype=torch.float32)
+        else:
+            self.constant_adj_matrix = torch.eye(nnodes, device=device, dtype=torch.float32)
+
+        for i in range(self.ngnn):
+            if gsllayer == 'relu':
+                self.graph_struct.append(Graph_ReLu_W(nnodes, device))
+            elif gsllayer == 'directed':
+                self.graph_struct.append(Graph_Directed_A(nnodes, window_size, alpha, k, device))
+            elif gsllayer == 'unidirected':
+                self.graph_struct.append(Graph_Uni_Directed_A(nnodes, window_size, alpha, k, device))
+            elif gsllayer == 'undirected':
+                self.graph_struct.append(Graph_Undirected_A(nnodes, window_size, alpha, k, device))
+            elif gsllayer == 'constant':
+                self.graph_struct.append(ConstantGraph(self.constant_adj_matrix))
+
+            else:
+                print('Wrong name of graph structure learning layer!')
+            self.conv1.append(GCNLayer(window_size, nhidden))
+            self.bnorm1.append(nn.BatchNorm1d(nnodes))
+            self.conv2.append(GCNLayer(nhidden, nhidden))
+            self.bnorm2.append(nn.BatchNorm1d(nnodes))
+
+        self.fc = nn.Linear(ngnn * nhidden, out_channels)
+
+    def forward(self, X):
+
+        X = X.to(self.device)
+
+        for i in range(self.ngnn):
+            self.adj[i] = self.graph_struct[i](self.idx)
+            self.adj[i] = self.adj[i] * self.z
+            self.h[i] = self.conv1[i](self.adj[i], X).relu()
+            self.h[i] = self.bnorm1[i](self.h[i])
+            self.skip[i], _ = torch.min(self.h[i], dim=1)
+            self.h[i] = self.conv2[i](self.adj[i], self.h[i]).relu()
+            self.h[i] = self.bnorm2[i](self.h[i])
+            self.h[i], _ = torch.min(self.h[i], dim=1)
+            self.h[i] = self.h[i] + self.skip[i]
+
+        h = torch.cat(self.h, 1)
+        output = self.fc(h)
+
+        return output
+
+    def get_adj(self):
+        return self.adj
